6ddfaf29899d17b0b6aef53a8ffeceaa7b03d465
Santiago <uo265578@uniovi.es>
2020-11-26 16:42:24 +0100

    Primer commit

---
+++ Parrafos.js
@@ -0,0 +1,21 @@
+document.write("<p>Informe de la asignatura: ");
+document.write(asignatura.nombreAsignatura);
+document.write("</p>");
+document.write("<p>Titulación: ");
+document.write(asignatura.titulacion);
+document.write("</p>");
+document.write("<p>Centro: ");
+document.write(asignatura.centro);
+document.write("</p>");
+document.write("<p>Universidad: ");
+document.write(asignatura.universidad);
+document.write("</p>");
+document.write("<p>Curso actual: ")
+document.write(asignatura.curso)
+document.write("</p>");
+document.write("<p>Nombre del estudiante: ")
+document.write(asignatura.estudiante)
+document.write</"p">
+document.write("<p>E-mail: ")
+document.write(asignatura.email)
+document.write</"p">
